f171bfd16a15a5b19bf1585467c58289998110be
adamkjall <adam.kjall@hotmail.com>
2020-12-01 18:24:13 +0100

    forced footer to always be at the bottom of the page, added a projects page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,15 @@ import Footer from "@components/footer/Footer";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen relative bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+    <div className="relative flex flex-col min-h-screen w-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="max-w-screen-xl mx-auto px-4 py-12">{children}</main>
+      <main className="max-w-screen-xl mx-auto mb-auto px-4 py-12">
+        {children}
+      </main>
       <Footer />
     </div>
   );
---
+++ src/pages/projects/index.js
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">Projects page under contruction</h1>
+    </div>
+  );
+}
