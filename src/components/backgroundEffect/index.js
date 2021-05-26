1b6bcc4ec38084a6d347b81f758b526421f32ea4
adamkjall <adam.kjall@hotmail.com>
2021-05-26 20:38:03 +0200

    save

--- src/components/backgroundEffect/index.js
+++ src/components/backgroundEffect/index.js
@@ -24,52 +24,8 @@ export default function BackgroundEffect() {
   });
 
   useEffect(() => {
-    // Init
     // const rand = Math.random();
     setAspect(window.innerWidth / window.innerHeight);
-    // const scene = new THREE.Scene();
-
-    // const uniforms = THREE.UniformsUtils.merge([
-    //   THREE.UniformsLib["ambient"],
-    //   THREE.UniformsLib["lights"],
-    //   THREE.ShaderLib.phong.uniforms,
-    //   { time: { type: "f", value: 0 } },
-    // ]);
-
-    // const material = new THREE.ShaderMaterial({
-    //   uniforms: uniforms,
-    //   vertexShader: sphereVertShader,
-    //   fragmentShader: sphereFragShader,
-    //   lights: true,
-    // });
-
-    // const geometry = new THREE.SphereBufferGeometry(10, 64, 64);
-    // const sphere = new THREE.Mesh(geometry, material);
-
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // camera.position.z = 52;
-
-    // scene.add(sphere);
-    // sphere.position.z = 0;
-    // sphere.modifier = rand;
-
-    // Light
-
-    // scene.background = new THREE.Color(0x111111);
-
-    // const light = new THREE.DirectionalLight(0xfffffff, 0.6);
-    // light.position.z = 200;
-    // light.position.x = 100;
-    // light.position.y = 100;
-    // scene.add(light);
-    // const light2 = new THREE.DirectionalLight(0xffffff, 0.6);
-    // light2.position.z = 100;
-    // light2.position.x = -200;
-    // light2.position.y = -100;
-    // scene.add(light2);
-
-    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
-    // scene.add(ambientLight);
 
     if (typeof window === "object") {
       // Check if document is finally loaded
@@ -80,24 +36,6 @@ export default function BackgroundEffect() {
 
   const handleResize = () => {
     setAspect(window.innerWidth / window.innerHeight);
-    // const canvasHeight = window.innerHeight;
-    // const windowWidth = window.innerWidth;
-    // const fullHeight = canvasHeight + canvasHeight * 0.3;
-    // canvasRef.current.style.height = fullHeight;
-    // renderer.setSize(windowWidth, fullHeight);
-    // camera.aspect = windowWidth / fullHeight;
-    // camera.updateProjectionMatrix();
-
-    //   if (windowWidth <= 696) {
-    //     sphere.position.x = 14;
-    //     sphere.position.y = 10;
-    //   } else if (windowWidth <= 1024) {
-    //     sphere.position.x = 18;
-    //     sphere.position.y = 14;
-    //   } else {
-    //     sphere.position.x = 22;
-    //     sphere.position.y = 16;
-    //   }
   };
 
   return (
