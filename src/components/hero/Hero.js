a961ce761b70e464fb4ff4c60dadb6f54134f44e
adamkjall <adam.kjall@hotmail.com>
2021-06-02 19:59:03 +0200

    removed comments

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,16 +3,12 @@ export default function Hero() {
     <div className="h-screen mb-4 flex items-center text-white " style={{ backgroundColor: "#111111" }}>
       <div className="flex flex-col animate-fadeIn pl-8 lg:pl-32 ">
         <h1
-          //   data-scroll
-          //   data-scroll-speed="5"
           className="text-2xl lg:text-4xl uppercase tracking-widest leading-tight -mb-8 text-gray-300"
         >
           Adam Kjäll
         </h1>
         <br />
         <h1
-          //   data-scroll
-          //   data-scroll-speed="6"
           className="text-4xl lg:text-7xl tracking-wider"
         >
           new WebDeveloper()
